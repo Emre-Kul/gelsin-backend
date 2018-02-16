e68704aff0dcd651157e8df5bc969459c1f1e52a
Emre-Kul <emrekul35@gmail.com>
2018-02-16 23:19:03 +0300

    mongo connection established

---
+++ config.js
@@ -0,0 +1,5 @@
+//Note: MONGO_URL shouldn't be public but this is an example project.
+module.exports = {
+    MONGO_URL: "mongodb://kinguser:kingpassword@ds237858.mlab.com:37858/gelsin-db"
+}
+
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const CONFIG = require('./config.js');
 
 require('./routes/index.js')(app);
+
+mongoose.connect(CONFIG.MONGO_URL);
 
 const server = app.listen(8080 || process.env.PORT, () => {
     const host = server.address().address;
