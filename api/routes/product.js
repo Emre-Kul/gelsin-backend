a05b510d32fcb14a952cafb56d814f438bd6ae7f
Emre-Kul <emrekul35@gmail.com>
2018-02-17 14:07:44 +0300

    product error handling improved

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product.js');
 module.exports = function (app) {
-    
+
     app.get('/product', (req, res) => {
         Product.getProducts().
             then((data) => res.status(200).json({ "data": data })).
@@ -8,16 +8,21 @@ module.exports = function (app) {
     });
 
     app.post('/product', (req, res) => {
-        let product = new Product({
-            'name': req.body.name,
-            'price': req.body.price,
-            'shop': req.body.shop
-        });
-        product.saveProduct().
-            then((data) => res.status(200).json({ "data": data })).
-            catch((err) => res.status(500).json({ "error": err }));
+        let params = req.body;
+        if (typeof params.name == "undefined" || typeof params.price == "undefined" || typeof params.shop == "undefined")
+            res.status(400).json({ "error": { "message": "Bad Request" } });
+        else {
+            let product = new Product({
+                'name': req.body.name,
+                'price': req.body.price,
+                'shop': req.body.shop
+            });
+            product.saveProduct().
+                then((data) => res.status(200).json({ "data": data })).
+                catch((err) => res.status(500).json({ "error": err }));
+        }
     });
-    
+
     app.get('/product/:_id', (req, res) => {
         Product.getProduct(req.params._id).
             then((data) => res.status(200).json({ "data": data })).
